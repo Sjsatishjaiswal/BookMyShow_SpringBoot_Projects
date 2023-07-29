61b24ccbaf2334d0930ac724c040638bf87387f7
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-07-29 16:02:49 +0530

    BOOK My SHOW SPRINGBOOT FRAMWORK PROJECTS

---
+++ src/main/java/com/example/book_my_show/Enums/Genre.java
@@ -0,0 +1,11 @@
+package com.example.book_my_show.Enums;
+
+
+public enum Genre {
+
+    FICTIONAL,
+    COMEDY,
+    ACTION,
+    HORROR
+}
+
---
+++ src/main/java/com/example/book_my_show/convetors/TicketConvertors.java
@@ -0,0 +1,17 @@
+package com.example.book_my_show.convetors;
+
+
+import com.example.book_my_show.Entities.TicketEntity;
+import com.example.book_my_show.EntryDtos.TicketEntryDto;
+
+public class TicketConvertors {
+
+
+    public static TicketEntity convertEntryToEntity(TicketEntryDto ticketEntryDto){
+
+        TicketEntity ticketEntity = new TicketEntity();
+        return ticketEntity;
+
+    }
+}
+
